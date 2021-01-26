75c657595f06f80d73d1424ed363d29fe1c21468
Zane Bode <zb882320@ohio.edu>
2021-01-26 11:00:58 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zane Bode
+ *        Date: 1/26
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
